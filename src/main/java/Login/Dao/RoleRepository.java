9a110c8b8817d9c149be1e743bfdbfdf8b3e3822
sungmin park <sungpark.inbox@gmail.com>
2021-01-28 15:01:25 -0500

    first

---
+++ src/main/java/Login/Dao/RoleRepository.java
@@ -0,0 +1,11 @@
+package Login.Dao;
+
+import Login.Entity.ERole;
+import Login.Entity.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/Login/Entity/ERole.java
@@ -0,0 +1,7 @@
+package Login.Entity;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
